2c87b0903655accd7439f5db9a59053ffe72bfd1
Mihaela Teodoriu <mihaela.teodoriu@yahoo.com>
2019-06-10 22:11:35 +0300

    Initial setup

---
+++ src/main/java/com/revianlabs/lutin/LutinApplication.java
@@ -0,0 +1,13 @@
+package com.revianlabs.lutin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LutinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LutinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revianlabs/lutin/LutinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.revianlabs.lutin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LutinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

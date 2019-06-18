d83cc5b264daf1c725406bd812884e379e4de677
Mihaela Teodoriu <44039398+mischha@users.noreply.github.com>
2019-06-18 15:54:18 +0300

    Feature/add model classes (#2)
    * added model classes(task, priority, category)

--- src/main/java/com/revianlabs/lutin/LutinApplication.java
+++ src/main/java/com/revianlabs/lutin/LutinApplication.java
@@ -10,4 +10,5 @@ public class LutinApplication {
 		SpringApplication.run(LutinApplication.class, args);
 	}
 
+
 }
---
+++ src/main/java/com/revianlabs/lutin/model/Category.java
@@ -0,0 +1,31 @@
+package com.revianlabs.lutin.model;
+
+public class Category {
+    private String id;
+    private String color;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
